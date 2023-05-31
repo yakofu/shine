c545e77f239a026e723837b1bdc7281c429397f2
yakofu <132476248+yakofu@users.noreply.github.com>
2023-05-31 15:15:35 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const swiper = new Swiper('.swiper', {
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  });
+
+function sliderMouseSlideInit() {
+	document.addEventListener("mousemove", function(e){
+		const targetElement = e.target;
+		if (targetElement.closest('[data-mousemove-swipe]')) {
+			const sliderElement = targetElement.closest('[data-mousemove-swipe]');
+			const sliderItem = swiper[getIndex(sliderElement)];
+			const sliderLength = sliderItem.slides.length;
+			if (sliderLength > 1){
+				const sliderWidth = sliderItem.width;
+				const sliderPath = Math.round(sliderWidth / sliderLength);
+				const sliderMousePos = e.clientX - sliderElement.offsetLeft;
+				const sliderSlide = Math.floor(sliderMousePos / sliderPath);
+				sliderItem.slideTo(sliderSlide);
+			}
+		}
+	})
+
+	function getIndex(el) {
+		return Array.from(el.parentNode.children).indexOf(el);
+	}
+}
+
+if (document.querySelector("[data-mousemove-swipe]")){
+	sliderMouseSlideInit()
+}
